524e4640e58bad360eb054316392014373498300
Delisma <delispence7@gmail.com>
2024-01-30 09:57:12 -0500

    added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+//const companyName  = 'Scuber'
+/*it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});*/
+
+
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
